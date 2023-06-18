ff14098f4453df37fde1b51833fc3bc10eabc4b0
Amir <amirmalul546@gmail.com>
2023-06-18 17:26:36 +0300

    asd

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,59 @@
-import React from "react";
-import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const NavBar = () => {
-    return (
-      <Navbar expand="lg">
+
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
+  return (
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
-        <Navbar.Brand href="#">Logo</Navbar.Brand>
-        <img src={} alt="Logo" />
-        <Navbar.Toggle aria-controls="basic-navbar-nav" > ss</NavBar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <NavDropdown title="asd" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#service1">Service 1</NavDropdown.Item>
-              <NavDropdown.Item href="#service2">Service 2</NavDropdown.Item>
-              <NavDropdown.Item href="#service3">Service 3</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#more">More Services</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a>
+              </div>
+            </span>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
-    );
-  };
+  )
+}
 
-  export default NavBar
-
+export default NavBar
