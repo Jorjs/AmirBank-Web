1f847a8ee9226cf26f9020918766792f4f78fb90
Amir <amirmalul546@gmail.com>
2023-06-21 23:10:51 +0300

    asdasd

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -23,12 +23,8 @@ export const Slider = () => {
       </Carousel.Item>
       <Carousel.Item>
       <div className="slider_container">
-      <img className="d-block w-100 slider-img" src={img2} alt="Second slide"/>
+      <img src={img2} alt="Second slide"/>
       </div>
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
